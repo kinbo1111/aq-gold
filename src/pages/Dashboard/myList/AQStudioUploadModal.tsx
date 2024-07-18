1dc285853fc81882ee41fa8235a3d817b644571c
codegenius2 <lucifer231220@gmail.com>
2024-07-18 12:57:34 +0900

    Fix: translation

--- src/pages/Dashboard/myList/AQStudioUploadModal.tsx
+++ src/pages/Dashboard/myList/AQStudioUploadModal.tsx
@@ -10,7 +10,7 @@ const AQStudioUploadModal = () => {
             <div className="max-w-[530px] w-full b-gray-500 rounded-[10px] py-14 flex items-center justify-center flex-col">
                 <h6 className="text-center gray-200 sub-2r mb-6">{t("Let’s upload and share your 360° video contents.")}</h6>
                 <Button
-                    label="AQ Studio"
+                    label={t("btn")}
                     onClick={() => {}}
                     iconExist
                     full
