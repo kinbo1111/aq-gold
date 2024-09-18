12324c2fb493affaacca60545dac213936013705
codegenius2 <lucifer231220@gmail.com>
2024-09-18 11:53:33 +0900

    Fix: fixed the movie item for watching in dashboard

--- src/pages/Dashboard/MovieItem.tsx
+++ src/pages/Dashboard/MovieItem.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import VideoModal from '../../components/VideoModal';
+import { useNavigate } from "react-router-dom";
 
-interface MovieItemProps {
+export type MovieItemProps = {
+    videoId: string;
     imgSrc: string;
+    videoUrl: string;
 }
 
+export type VideoProps = {
+  id: string;
+  videoUrl: string;
+  thumbnailUrl?: string;
+  updatedAt?: string; // Make this optional
+};
+
+
 const MovieItem: React.FC<MovieItemProps> = ({
-    imgSrc
+    videoId,
+    imgSrc,
+    videoUrl
 }) => {
+    const navigate = useNavigate();
+
+    const handleShow = () => {
+        navigate(`/video/${videoId}`, { state: { videoUrl:videoUrl } });
+    };
+
     return (
+    <>
         <div className="w-full">
-            <img src={imgSrc} alt="" className="w-full" />
+            <img src={imgSrc} alt="" className="w-full" onClick={handleShow} />
         </div>
+    </>
     );
 };
 
